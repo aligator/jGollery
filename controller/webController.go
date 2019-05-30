153760c8bbeafaba9d5143bcc790034e4522b051
aligator <aligator@suncraft-server.de>
2019-05-30 17:12:48 +0200

    web controller refactoring

--- controller/webController.go
+++ controller/webController.go
@@ -6,6 +6,7 @@ import (
 	"jGollery/entity"
 	"log"
 	"net/http"
+	"regexp"
 )
 
 type WebController struct {
@@ -13,23 +14,33 @@ type WebController struct {
 	galleryPath string
 }
 
+// Controller for all webservices of jGollery
+// staticPath is the folder where all static files are located. Such as the gallery-images or stylesheets.
+// galleryPath is the folder inside of staticPath, where the folders for the galleries are located.
 func NewWebController(staticPath string, galleryPath string) *WebController {
 	wc := WebController{
 		staticPath,
 		galleryPath,
 	}
-	err := wc.init()
-	if err != nil {
-		log.Fatal(err)
-	}
+	if wc.check(staticPath) && wc.check(galleryPath) {
+		err := wc.init()
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	return &wc
+		return &wc
+	} else {
+		log.Fatal("given paths are not valid", staticPath, galleryPath)
+		return nil
+	}
 }
 
+// run the webserver
 func (wc *WebController) Run(addr string) {
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
+// setup static fileserver and dynamic webserver for jGollery
 func (wc *WebController) init() error {
 	if err := wc.setupStatic(wc.staticPath); err != nil {
 		return err
@@ -42,19 +53,17 @@ func (wc *WebController) init() error {
 	return nil
 }
 
+// setups a fileserver which serves all files inside the given path.
 func (wc *WebController) setupStatic(path string) error {
-	if wc.check(path) && wc.check(wc.staticPath) {
-		fs := http.FileServer(http.Dir(path))
-		fullPath := "/" + path + "/"
+	fs := http.FileServer(http.Dir(path))
+	fullPath := "/" + path + "/"
 
-		http.Handle(fullPath, http.StripPrefix(fullPath, fs))
-		return nil
-	}
-	return errors.New(path + " is not allowed")
+	http.Handle(fullPath, http.StripPrefix(fullPath, fs))
+	return nil
 }
 
+// setups the webserver which handles gallery requests. Uses the paths provided by the webController.
 func (wc *WebController) setupDynamic() error {
-
 	// load all galleries and serve them
 	path := wc.staticPath + "/" + wc.galleryPath
 
@@ -74,25 +83,27 @@ func (wc *WebController) setupDynamic() error {
 		if fileInfo.IsDir() {
 
 			// serve gallery
-			if wc.check(wc.galleryPath) {
-				pics := entity.NewPicFiles(path, fileInfo.Name())
-				gallery, err := entity.NewGallery(pics)
+			pics := entity.NewPicFiles(path, fileInfo.Name())
+			gallery, err := entity.NewGallery(pics)
 
-				if err == nil {
-					http.Handle("/"+fileInfo.Name()+"/", gallery)
-				} else {
-					return errors.New("could not set up gallery")
-				}
+			if err == nil {
+				http.Handle("/"+fileInfo.Name()+"/", gallery)
 			} else {
-				return errors.New(wc.galleryPath + " is not allowed")
+				return errors.New("could not set up gallery")
 			}
+		} else {
+			return errors.New(wc.galleryPath + " is not allowed")
 		}
 	}
 
 	return nil
 }
 
+const pathRegexp = "^[\\/a-zA-Z0-9\\-_ äöü]+$"
+
+var compPathRegexp = regexp.MustCompile(pathRegexp)
+
+// checks if path is valid
 func (wc *WebController) check(path string) bool {
-	// TODO
-	return true
+	return compPathRegexp.MatchString(path)
 }
