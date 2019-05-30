3cb708c61ff49477aa18f5a370e06d8d14eb53af
Dominik Braun <braun@sternentstehung.de>
2019-05-30 22:56:12 +0200

    implemented command line flag for listening address

--- cmd/jGolleryServer/main.go
+++ cmd/jGolleryServer/main.go
@@ -1,8 +1,14 @@
 package main
 
-import . "jGollery/controller"
+import (
+	"flag"
+	. "jGollery/controller"
+)
 
 func main() {
+	addr := flag.String("addr", ":8080", `The address the application servers listens to`)
+	flag.Parse()
+
 	webController := NewWebController("static", "gallery")
-	webController.Run(":8080")
+	webController.Run(*addr)
 }
